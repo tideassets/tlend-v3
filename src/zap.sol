b4d252feedc5c770d972846d6a9877a1cc1eda63
w <wangchh@gmail.com>
2023-11-25 10:08:07 +0800

    first commit

---
+++ src/zap.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2023
+// zap.sol : from radiant zapping, use your native token and TTL to add liquidity to tswap pools
+//
+
+pragma solidity ^0.8.20;
+
+contract Zap {}
