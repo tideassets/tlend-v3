8b18415e73199aad7de27fb786de4a23232de5d8
w <wangchh@gmail.com>
2023-11-27 12:03:12 +0800

    fix some errors

--- src/ttl.sol
+++ src/ttl.sol
@@ -1,16 +1,11 @@
 // SPDX-License-Identifier: MIT
 // Copyright (C) 2023
 // ttl.sol : ttl token for rewards distribution
-//
-
-// SPDX-License-Identifier: MIT
-// Copyright (C) 2023
-// ttl.sol : ttl token for rewards distribution
 
 pragma solidity ^0.8.20;
 
 import "@tide/tfund/token.sol";
 
 contract TLLToken is TToken {
-  constructor() TfundToken("tide TTL", "TTL") {}
+  constructor(address lzend) TToken(lzend, "tide TTL", "TTL") {}
 }
